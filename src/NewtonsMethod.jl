7cde23f25d50a3a5469492d481aad00c701a4849
Yi-Kai Kan <tw.ykkan@gmail.com>
2022-03-08 19:45:12 +0100

    Add newtonroot and test

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,24 @@
 module NewtonsMethod
 
-# Write your package code here.
+using ForwardDiff
 
+function newtonroot(f, fp; x0, tol=1e-7, maxiter=1000)
+    abserror = Inf
+    iter = 1
+    x = x0
+    while abserror > tol && iter <= maxiter
+        x_new = x - f(x)/fp(x)
+        iter = iter +1
+        abserror = abs(x_new - x)
+        x = x_new
+    end
+    return x
+end
+
+function newtonroot(f; x0, tol = 1e-7, maxiter=1000)
+    fp = x -> ForwardDiff.derivative(f, x)
+    newtonroot(f, fp; x0=x0, tol=tol, maxiter=maxiter)
+end
+
+export newtonroot
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using NewtonsMethod
 using Test
 
-@testset "NewtonsMethod.jl" begin
-    # Write your tests here.
+@testset "newtonroot" begin
+    f(x) = x^2
+    fp(x) = 2*x
+    @test newtonroot(f, fp; x0=1.0) ≈ 0.0 atol=1.0e-7
+    @test newtonroot(f; x0=1.0) ≈ 0.0 atol=1.0e-7
 end
